ab95df7370f77012306f79b0cdfa2beb604cf2ae
Umar Esso <umarkhatab467@gmail.com>
2022-08-27 13:29:58 +0500

    added introduction

--- components/Introduction.jsx
+++ components/Introduction.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import styles from "../styles/Introduction.module.css";
 
+
 function Introduction() {
     return (
-        <div>Introduction</div>
+        <div className={styles.introduction}>
+            <div className={styles.introduction__title}>
+                Unify <p className={styles.gradient}> your Identity</p>
+            </div>
+            <div className={styles.introduction__description}>
+                Synergy is all about bringing your all social media reputation at one place
+
+
+            </div>
+
+
+        </div>
+
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Introduction from '../components/Introduction'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Navbar />
+      <Introduction />
     </div>
   )
 }
